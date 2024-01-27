051afd73e008db0cbe578ecbcb6b3866b86b0a9f
Arthur <arthurdidier10@gmail.com>
2024-01-26 23:22:58 -0600

    review2

--- src/componets/TodoUpdate.jsx
+++ src/componets/TodoUpdate.jsx
@@ -1,46 +1,102 @@
-import { useRef, useState } from 'react';
+import { useState, useRef } from 'react';
 import { FaEdit } from 'react-icons/fa';
+import { Modal, Button } from 'react-bootstrap';
 import { useForm } from '../Hooks/useForm';
 
 export const TodoUpdate = ({ todo, handleUpdateTodo }) => {
-	const { updateDescription, onInputChange } = useForm({
-		updateDescription: todo.description,
-	});
+  const { updateName, updateDescription, updateDate, onInputChange } = useForm({
+    updateName: todo.name,
+    updateDescription: todo.description,
+    updateDate: todo.date,
+  });
 
-	const [disabled, setDisabled] = useState(true);
-	const focusInputRef = useRef();
+  const [showModal, setShowModal] = useState(false);
+  const focusInputRef = useRef();
 
-	const onSubmitUpdate = e => {
-		e.preventDefault();
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
 
-		const id = todo.id;
-		const description = updateDescription;
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
-		handleUpdateTodo(id, description);
+  const onSubmitUpdate = (e) => {
+    e.preventDefault();
 
-		setDisabled(!disabled);
+    const id = todo.id;
+    const name = updateName;
+    const description = updateDescription;
+    const date = updateDate;
 
-		focusInputRef.current.focus();
-	};
+    handleUpdateTodo(id, name, description, date);
 
-	return (
-		<form onSubmit={onSubmitUpdate}>
-			<input
-				type='text'
-				className={`input-update ${
-					todo.done ? 'text-decoration-dashed' : ''
-				}`}
-				name='updateDescription'
-				value={updateDescription}
-				onChange={onInputChange}
-				placeholder='¿Qué hay que hacer?'
-				readOnly={disabled}
-				ref={focusInputRef}
-			/>
+    focusInputRef.current.focus();
+    handleCloseModal();
+  };
 
-			<button className='btn-edit' type='submit'>
-				<FaEdit />
-			</button>
-		</form>
-	);
+  return (
+    <>
+      <form onSubmit={onSubmitUpdate}>
+	  <input
+          type='text'
+          className={`input-update ${
+            todo.done ? 'text-decoration-dashed' : ''
+          }`}
+          name='updateDescription'
+          value={updateDescription}
+          onChange={onInputChange}
+          placeholder='Descripción de la Tarea'
+          readOnly={false}
+        />
+        <input
+          type='text'
+          className={`input-update ${
+            todo.done ? 'text-decoration-dashed' : ''
+          }`}
+          name='updateName'
+          value={updateName}
+          onChange={onInputChange}
+          placeholder='Descipcion de la tarea'
+          readOnly={false}
+        />
+        
+        <input
+          type='date'
+          className={`input-update ${
+            todo.done ? 'text-decoration-dashed' : ''
+          }`}
+          name='updateDate'
+          value={updateDate}
+          onChange={onInputChange}
+          placeholder='Fecha de la Tarea'
+          readOnly={false}
+        />
+
+        <button className='btn-edit' type='button' onClick={handleOpenModal}>
+          <FaEdit />
+        </button>
+      </form>
+
+      {/* Modal para mostrar detalles de la tarea */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Detalles de la Tarea</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p><strong>Nombre:</strong> {updateName}</p>
+          <p><strong>Descripción:</strong> {updateDescription}</p>
+          <p><strong>Fecha:</strong> {updateDate}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleCloseModal}>
+            Cerrar
+          </Button>
+          <Button variant='primary' onClick={onSubmitUpdate}>
+            Guardar Cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 };
